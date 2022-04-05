7006cc1281f3652e9e32a2760bb757a28f87ee24
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-05 15:20:37 -0300

    added method interface on base class to get object by its id

--- GarageVolver.Domain/Interfaces/IBaseService.cs
+++ GarageVolver.Domain/Interfaces/IBaseService.cs
@@ -5,5 +5,6 @@ namespace GarageVolver.Domain.Interfaces
     public interface IBaseService<TEntity> where TEntity : BaseEntity
     {
         public Task<List<TOutputModel>> GetAll<TOutputModel>() where TOutputModel : class;
+        public Task<TOutputModel> GetById<TOutputModel>(int id) where TOutputModel : class;
     }
 }
