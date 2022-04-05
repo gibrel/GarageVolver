c7070b5ac866305785ae5e60eb807ff740d11fd2
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-05 14:57:42 -0300

    added interface for truck service

---
+++ GarageVolver.Domain/Interfaces/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿using GarageVolver.Domain.Entities;
+
+namespace GarageVolver.Domain.Interfaces
+{
+    public interface IBaseService<TEntity> where TEntity : BaseEntity
+    {
+        public Task<List<TOutputModel>> GetAll<TOutputModel>() where TOutputModel : class;
+    }
+}
