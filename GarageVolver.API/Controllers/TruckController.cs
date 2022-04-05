006dd11fb07ec440a691524780f959a532420ba5
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-05 14:58:56 -0300

    implemented controller to run get trucks tests

---
+++ GarageVolver.API/Controllers/TruckController.cs
@@ -0,0 +1,42 @@
+﻿using GarageVolver.API.Models;
+using GarageVolver.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GarageVolver.API.Controllers
+{
+    /// <summary>
+    /// Class <c>TruckController</c> handles API requests for truck registration solution.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class TruckController : ControllerBase
+    {
+        private readonly ITruckService _truckService;
+
+        /// <summary>
+        /// Constructor for <c>TruckController</c> that uses <c>TruckService</c>
+        /// for record management.
+        /// </summary>
+        /// <param name="truckService"></param>
+        public TruckController(
+            ITruckService truckService)
+        {
+            _truckService = truckService;
+        }
+
+
+        [HttpGet("GetTrucks")]
+        public async Task<IActionResult> Get()
+        {
+            var customers = await _truckService.GetAll<GetTruckModel>();
+
+            if (customers.Any())
+            {
+                return Ok(customers);
+            }
+
+            return NotFound();
+        }
+
+    }
+}
