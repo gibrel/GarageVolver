5c47a41f35d3356a4b568426b9c6fc14ad1a133f
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-05 14:58:36 -0300

    added model to get trucks

---
+++ GarageVolver.API/Models/GetTruckModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GarageVolver.API.Models
+{
+    public class GetTruckModel
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public int ManufacturingYear { get; set; }
+        public int ModelYear { get; set; }
+    }
+}
