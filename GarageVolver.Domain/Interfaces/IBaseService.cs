bf757042498b1fc623131da49f714c434598fde9
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-05 16:27:29 -0300

    added interface for add method

--- GarageVolver.Domain/Interfaces/IBaseService.cs
+++ GarageVolver.Domain/Interfaces/IBaseService.cs
@@ -1,9 +1,14 @@
-﻿using GarageVolver.Domain.Entities;
+﻿using FluentValidation;
+using GarageVolver.Domain.Entities;
 
 namespace GarageVolver.Domain.Interfaces
 {
     public interface IBaseService<TEntity> where TEntity : BaseEntity
     {
+        public Task<TOutputModel> Add<TInputModel, TOutputModel, TValidator>(TInputModel inputModel)
+            where TValidator : AbstractValidator<TEntity>
+            where TInputModel : class
+            where TOutputModel : class;
         public Task<List<TOutputModel>> GetAll<TOutputModel>() where TOutputModel : class;
         public Task<TOutputModel> GetById<TOutputModel>(int id) where TOutputModel : class;
     }
