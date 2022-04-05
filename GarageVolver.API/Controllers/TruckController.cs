306f918ae560cbb6976edcb2bc7d499bac151d0e
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-05 15:21:03 -0300

    corrected typos and implemented endpoint to get truck by its id

--- GarageVolver.API/Controllers/TruckController.cs
+++ GarageVolver.API/Controllers/TruckController.cs
@@ -24,18 +24,42 @@ namespace GarageVolver.API.Controllers
             _truckService = truckService;
         }
 
-
+        /// <summary>
+        /// Enpoint responsible to retrieve a list of all trucks.
+        /// </summary>
+        /// <returns>List of <c>Truck</c> class objects, enveloped in <c>ActionResult</c>.</returns>
         [HttpGet("GetTrucks")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<GetTruckModel>))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> Get()
         {
-            var customers = await _truckService.GetAll<GetTruckModel>();
+            var trucks = await _truckService.GetAll<GetTruckModel>();
 
-            if (customers.Any())
+            if (trucks.Any())
             {
-                return Ok(customers);
+                return Ok(trucks);
             }
 
             return NotFound();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>List of <c>Truck</c> class objects, enveloped in <c>ActionResult</c>.</returns>
+        [HttpGet("GetTruckById")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GetTruckModel))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get(int id)
+        {
+            var truck = await _truckService.GetById<GetTruckModel>(id);
+
+            if (truck != null)
+            {
+                return Ok(truck);
+            }
+
+            return NotFound($"Could not find truck with id:{id}");
         }
 
     }
