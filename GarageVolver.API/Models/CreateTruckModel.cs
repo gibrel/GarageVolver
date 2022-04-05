78714e7efaf27b8fad2477b34f688d45f0c82b99
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-05 16:28:10 -0300

    created model for creating truck

---
+++ GarageVolver.API/Models/CreateTruckModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GarageVolver.API.Models
+{
+    public class CreateTruckModel
+    {
+        public string Model { get; set; }
+        public int ManufacturingYear { get; set; }
+        public int ModelYear { get; set; }
+    }
+}
