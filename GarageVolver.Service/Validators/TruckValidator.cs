0cca87ad0e093132a9d7a00e53ef3987f66f72a8
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-05 16:26:52 -0300

    implemented validator for truck entity

---
+++ GarageVolver.Service/Validators/TruckValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using GarageVolver.Domain.Entities;
+
+namespace GarageVolver.Service.Validators
+{
+    public class TruckValidator : AbstractValidator<Truck>
+    {
+        public TruckValidator()
+        {
+            RuleFor(t => t.Model)
+                .NotEmpty().WithMessage("Please insert truck model.")
+                .NotNull().WithMessage("Please insert truck model.");
+
+            RuleFor(t => t.ModelYear)
+                .NotEmpty().WithMessage("Please insert model year.")
+                .NotNull().WithMessage("Please insert model year.");
+
+            RuleFor(t => t.ManufacturingYear)
+                .NotEmpty().WithMessage("Please insert manufacturing year.")
+                .NotNull().WithMessage("Please insert manufacturing year.");
+        }
+    }
+}
