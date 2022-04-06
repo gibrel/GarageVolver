c6eda6179989fd71beb9f9ec043ee67723a54cc4
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-05 22:19:02 -0300

    aded update truck model

---
+++ GarageVolver.API/Models/UpdateTruckModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GarageVolver.API.Models
+{
+    public class UpdateTruckModel
+    {
+        public string Model { get; set; }
+        public int ManufacturingYear { get; set; }
+        public int ModelYear { get; set; }
+    }
+}
