78b4bd97d987300c6eeaf669e8d7b86c5ae6afde
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-05 22:18:49 -0300

    added interface for update on base service interface

--- GarageVolver.Domain/Interfaces/IBaseService.cs
+++ GarageVolver.Domain/Interfaces/IBaseService.cs
@@ -5,11 +5,16 @@ namespace GarageVolver.Domain.Interfaces
 {
     public interface IBaseService<TEntity> where TEntity : BaseEntity
     {
-        public Task<TOutputModel> Add<TInputModel, TOutputModel, TValidator>(TInputModel inputModel)
+        public abstract Task<TOutputModel> Add<TInputModel, TOutputModel, TValidator>(TInputModel inputModel)
             where TValidator : AbstractValidator<TEntity>
             where TInputModel : class
             where TOutputModel : class;
-        public Task<List<TOutputModel>> GetAll<TOutputModel>() where TOutputModel : class;
-        public Task<TOutputModel> GetById<TOutputModel>(int id) where TOutputModel : class;
+        public abstract Task<List<TOutputModel>> GetAll<TOutputModel>() where TOutputModel : class;
+        public abstract Task<TOutputModel> GetById<TOutputModel>(int id) where TOutputModel : class;
+        public abstract Task<TOutputModel> Update<TInputModel, TOutputModel, TValidator>(TInputModel inputModel)
+            where TValidator : AbstractValidator<TEntity>
+            where TInputModel : class
+            where TOutputModel : class;
+        public abstract Task<bool> Delete(int id);
     }
 }
