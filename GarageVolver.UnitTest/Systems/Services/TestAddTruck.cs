b0f7c6682327dd86db73cdafda78a82abb9c3fd1
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-06 23:02:11 -0300

    added service test to add truck

---
+++ GarageVolver.UnitTest/Systems/Services/TestAddTruck.cs
@@ -0,0 +1,133 @@
+﻿using AutoFixture.Xunit2;
+using AutoMapper;
+using FluentAssertions;
+using GarageVolver.API.Models;
+using GarageVolver.API.Configurations;
+using GarageVolver.Domain.Entities;
+using GarageVolver.Domain.Helpers;
+using GarageVolver.Domain.Interfaces;
+using GarageVolver.Service.Services;
+using GarageVolver.Service.Validators;
+using GarageVolver.UnitTest.Fixtures;
+using Moq;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GarageVolver.UnitTest.Systems.Services
+{
+    public class TestAddTruck
+    {
+        private Mapper ConfigureMapper()
+        {
+            var truckMapProfile = new TruckMapProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(truckMapProfile));
+            return new Mapper(configuration);
+        }
+
+        [Theory]
+        [AutoDomainData]
+        public async Task AddTruck_OnSucess_ReturnsGetTruckModel(
+            [Frozen] Mock<ITruckRepository> mockTruckRepository,
+            [Range(0, 1)] int truckModelId,
+            [Range(0, 1)] int truckModelYear)
+        {
+            IMapper mapper = ConfigureMapper();
+            truckModelYear += DateTime.Now.Year;
+            CreateTruckModel newTruck = new()
+            {
+                Model = Enumeration.GetById<TruckModel>(truckModelId).Name,
+                ModelYear = truckModelYear,
+                ManufacturingYear = DateTime.Now.Year
+            };
+            Truck insertTruck = new()
+            {
+                Id = 0,
+                Model = Enumeration.GetByName<TruckModel>(newTruck.Model),
+                ManufacturingYear = newTruck.ManufacturingYear,
+                ModelYear = newTruck.ModelYear,
+            };
+            mockTruckRepository
+                .Setup(repo => repo.Insert(insertTruck))
+                .ReturnsAsync(true);
+            var sut = new TruckService(
+                mockTruckRepository.Object,
+                mapper);
+
+            var result = await sut.Add<CreateTruckModel, GetTruckModel, TruckValidator>(newTruck);
+
+            result.Should().BeOfType<GetTruckModel>();
+        }
+
+        //[Theory]
+        //[AutoDomainData]
+        //public async Task AddTruck_OnSucess_InvokesTruckRepositoryOnce(
+        //    [Frozen] Mock<ITruckRepository> mockTruckRepository,
+        //    [Range(0, 1)] int truckModelId,
+        //    [Range(0, 1)] int truckModelYear)
+        //{
+        //    IMapper mapper = ConfigureMapper();
+        //    truckModelYear += DateTime.Now.Year;
+        //    CreateTruckModel newTruck = new()
+        //    {
+        //        Model = Enumeration.GetById<TruckModel>(truckModelId).Name,
+        //        ModelYear = truckModelYear,
+        //        ManufacturingYear = DateTime.Now.Year
+        //    };
+        //    Truck insertTruck = new()
+        //    {
+        //        Id = 0,
+        //        Model = Enumeration.GetByName<TruckModel>(newTruck.Model),
+        //        ManufacturingYear = newTruck.ManufacturingYear,
+        //        ModelYear = newTruck.ModelYear,
+        //    };
+        //    mockTruckRepository
+        //        .Setup(repo => repo.Insert(insertTruck))
+        //        .ReturnsAsync(true);
+        //    var sut = new TruckService(
+        //        mockTruckRepository.Object,
+        //        mapper);
+
+        //    var result = await sut.Add<CreateTruckModel, GetTruckModel, TruckValidator>(newTruck);
+
+        //    mockTruckRepository.Verify(
+        //        repo => repo.Insert(insertTruck), Times.Once());
+        //}
+
+
+        //[Theory]
+        //[AutoDomainData]
+        //public async Task AddTruck_Failure_SomeError(
+        //    [Frozen] Mock<ITruckRepository> mockTruckRepository,
+        //    [Range(0, 1)] int truckModelId,
+        //    [Range(0, 1)] int truckModelYear)
+        //{
+        //    IMapper mapper = ConfigureMapper();
+        //    truckModelYear += DateTime.Now.Year;
+        //    CreateTruckModel newTruck = new()
+        //    {
+        //        Model = Enumeration.GetById<TruckModel>(truckModelId).Name,
+        //        ModelYear = truckModelYear,
+        //        ManufacturingYear = DateTime.Now.Year
+        //    };
+        //    Truck insertTruck = new()
+        //    {
+        //        Id = 0,
+        //        Model = Enumeration.GetByName<TruckModel>(newTruck.Model),
+        //        ManufacturingYear = newTruck.ManufacturingYear,
+        //        ModelYear = newTruck.ModelYear,
+        //    };
+        //    mockTruckRepository
+        //        .Setup(repo => repo.Insert(insertTruck))
+        //        .ReturnsAsync(true);
+        //    var sut = new TruckService(
+        //        mockTruckRepository.Object,
+        //        mapper);
+
+        //    var result = await sut.Add<CreateTruckModel, GetTruckModel, TruckValidator>(newTruck);
+
+        //    result.Should().BeNull();
+        //}
+    }
+}
