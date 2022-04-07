6d0ad300b970d3ae65a0f3ee02a8e7f42c98d794
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-06 23:10:14 -0300

    added service get all trucks test

---
+++ GarageVolver.UnitTest/Systems/Services/TestGetAllTrucks.cs
@@ -0,0 +1,46 @@
+﻿using AutoFixture.Xunit2;
+using AutoMapper;
+using FluentAssertions;
+using GarageVolver.API.Models;
+using GarageVolver.API.Configurations;
+using GarageVolver.Domain.Entities;
+using GarageVolver.Domain.Interfaces;
+using GarageVolver.Service.Services;
+using GarageVolver.UnitTest.Fixtures;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+using System.Collections.Generic;
+
+namespace GarageVolver.UnitTest.Systems.Services
+{
+    public class TestGetAllTrucks
+    {
+        private Mapper ConfigureMapper()
+        {
+            var truckMapProfile = new TruckMapProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(truckMapProfile));
+            return new Mapper(configuration);
+        }
+
+        [Theory]
+        [AutoDomainData]
+        public async Task GetAllTrucks_OnSucess_ReturnsListOfGetTruckModel(
+            [Frozen] Mock<ITruckRepository> mockTruckRepository,
+            List<Truck> getListTruckModel)
+        {
+            IMapper mapper = ConfigureMapper();
+            mockTruckRepository
+                .Setup(repo => repo.Select())
+                .ReturnsAsync(getListTruckModel);
+            var sut = new TruckService(
+                mockTruckRepository.Object,
+                mapper);
+
+            var result = await sut.GetAll<GetTruckModel>();
+
+            result.Should().BeOfType<List<GetTruckModel>>();
+        }
+
+    }
+}
