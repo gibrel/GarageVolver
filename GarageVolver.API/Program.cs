d36cfc21880b61de35ec3d7f175ba7fb647ba2a9
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-06 23:09:29 -0300

    changed map configuration

--- GarageVolver.API/Program.cs
+++ GarageVolver.API/Program.cs
@@ -1,6 +1,15 @@
+using AutoMapper;
+//using GarageVolver.API.Models;
+using GarageVolver.API.Configurations;
+using GarageVolver.Domain.Entities;
+using GarageVolver.Domain.Interfaces;
+using GarageVolver.Service.Services;
+using Newtonsoft.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+ConfigureServices(builder.Services);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,3 +32,19 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddSingleton(new MapperConfiguration(config =>
+    {
+        config.AddProfile<TruckMapProfile>();
+    }).CreateMapper());
+
+    //builder.Services.AddScoped<IBaseRepository<Truck>, BaseRepository<Truck>>();
+    builder.Services.AddScoped<IBaseService<Truck>, BaseService<Truck>>();
+    //builder.Services.AddScoped<ITruckRepository, TruckRepository>();
+    builder.Services.AddScoped<ITruckService, TruckService>();
+
+    services.AddMvc(option => option.EnableEndpointRouting = false)
+        .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+}
