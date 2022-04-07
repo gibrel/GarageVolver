8a90f63e46fa2162289b9f880b62971ae56f2102
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-06 23:04:58 -0300

    implemented truck service

---
+++ GarageVolver.Service/Services/TruckService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using GarageVolver.Domain.Entities;
+using GarageVolver.Domain.Interfaces;
+
+namespace GarageVolver.Service.Services
+{
+    public class TruckService : BaseService<Truck>, ITruckService
+    {
+        public TruckService(
+            ITruckRepository truckRepository,
+            IMapper mapper
+        ) : base(truckRepository, mapper)
+        {
+        }
+    }
+}
