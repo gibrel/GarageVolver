577d03273eec208cd3c86c6b33e75f754ede07eb
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-06 23:09:55 -0300

    added service delete trucks test

---
+++ GarageVolver.UnitTest/Systems/Services/TestDeleteTruck.cs
@@ -0,0 +1,63 @@
+﻿using AutoFixture.Xunit2;
+using AutoMapper;
+using FluentAssertions;
+using GarageVolver.API.Configurations;
+using GarageVolver.Domain.Entities;
+using GarageVolver.Domain.Interfaces;
+using GarageVolver.Service.Services;
+using GarageVolver.UnitTest.Fixtures;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GarageVolver.UnitTest.Systems.Services
+{
+    public class TestDeleteTruck
+    {
+        private Mapper ConfigureMapper()
+        {
+            var truckMapProfile = new TruckMapProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(truckMapProfile));
+            return new Mapper(configuration);
+        }
+
+        [Theory]
+        [AutoDomainData]
+        public async Task DeleteTruck_OnSucess_ReturnsTrue(
+            [Frozen] Mock<ITruckRepository> mockTruckRepository,
+            Truck toBeDeletedTruck)
+        {
+            IMapper mapper = ConfigureMapper();
+            mockTruckRepository
+                .Setup(repo => repo.Delete(toBeDeletedTruck.Id))
+                .ReturnsAsync(true);
+            var sut = new TruckService(
+                mockTruckRepository.Object,
+                mapper);
+
+            var result = await sut.Delete(toBeDeletedTruck.Id);
+
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [AutoDomainData]
+        public async Task DeleteTruck_Failure_ReturnsFalse(
+            [Frozen] Mock<ITruckRepository> mockTruckRepository,
+            Truck toBeDeletedTruck)
+        {
+            toBeDeletedTruck.Id = 0;
+            IMapper mapper = ConfigureMapper();
+            mockTruckRepository
+                .Setup(repo => repo.Delete(toBeDeletedTruck.Id))
+                .ReturnsAsync(false);
+            var sut = new TruckService(
+                mockTruckRepository.Object,
+                mapper);
+
+            var result = await sut.Delete(toBeDeletedTruck.Id);
+
+            result.Should().BeFalse();
+        }
+    }
+}
