97948084a31ad668f3806663a611b5b92154b72d
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-07 23:40:36 -0300

    added documentation to swagger

--- GarageVolver.API/Program.cs
+++ GarageVolver.API/Program.cs
@@ -8,6 +8,7 @@ using GarageVolver.Domain.Interfaces;
 using GarageVolver.Service.Services;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +18,13 @@ ConfigureServices(builder.Services);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
