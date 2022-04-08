5798a6c86247cad7fcd802767fc32156c9c3930a
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-07 22:44:28 -0300

    added migrations

---
+++ GarageVolver.Data/Migrations/20220407183925_Migration.001.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GarageVolver.Data.Migrations
+{
+    public partial class Migration001 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Truck",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedDate = table.Column<string>(type: "INTEGER", nullable: false),
+                    ManufacturingYear = table.Column<string>(type: "INTEGER", nullable: false),
+                    ModelYear = table.Column<string>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Truck", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Truck");
+        }
+    }
+}
