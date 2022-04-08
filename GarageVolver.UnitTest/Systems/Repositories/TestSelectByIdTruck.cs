173f7d33f75d5e243682649083039377bea7cd3f
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-07 22:45:04 -0300

    aded select by id tests

---
+++ GarageVolver.UnitTest/Systems/Repositories/TestSelectByIdTruck.cs
@@ -0,0 +1,71 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using GarageVolver.Domain.Entities;
+using GarageVolver.UnitTest.Fixtures;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+using GarageVolver.Data.Repositories;
+using GarageVolver.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GarageVolver.UnitTest.Systems.Repositories
+{
+    public class TestSelectByIdTruck
+    {
+
+        [Theory]
+        [AutoDomainData]
+        public async Task SelectByIdTruck_OnRun_InvokeSaveChangesOnce(
+            [Frozen] Mock<DbSet<Truck>> mockTruckSet,
+            [Frozen] Mock<SQLiteContext> mockSQLiteContext)
+        {
+            mockSQLiteContext
+                .Setup(context => context.Trucks)
+                .Returns(mockTruckSet.Object);
+            var sut = new TruckRepository(mockSQLiteContext.Object);
+            var truck = TruckFixture.GenerateTruck();
+
+            await sut.Insert(truck);
+            await sut.Select(truck.Id);
+
+            mockSQLiteContext.Verify(m => m.Set<Truck>(), Times.Exactly(2));
+        }
+
+        [Theory]
+        [AutoDomainData]
+        public async Task SelectByIdTruck_OnSucess_ShouldReturnTruck(
+            [Frozen] Mock<DbSet<Truck>> mockTruckSet,
+            [Frozen] Mock<SQLiteContext> mockSQLiteContext)
+        {
+            mockSQLiteContext
+                .Setup(context => context.Trucks)
+                .Returns(mockTruckSet.Object);
+            var sut = new TruckRepository(mockSQLiteContext.Object);
+            var truck = TruckFixture.GenerateTruck();
+
+            await sut.Insert(truck);
+            var result = await sut.Select(truck.Id);
+
+            result.Should().BeOfType<Truck>();
+        }
+
+        [Theory]
+        [AutoDomainData]
+        public async Task SelectByIdTruck_OnSucess_ShouldHaveSameId(
+            [Frozen] Mock<DbSet<Truck>> mockTruckSet,
+            [Frozen] Mock<SQLiteContext> mockSQLiteContext)
+        {
+            mockSQLiteContext
+                .Setup(context => context.Trucks)
+                .Returns(mockTruckSet.Object);
+            var sut = new TruckRepository(mockSQLiteContext.Object);
+            var truck = TruckFixture.GenerateTruck();
+
+            await sut.Insert(truck);
+            var result = await sut.Select(truck.Id);
+
+            result.Id.Should().Be(truck.Id);
+        }
+    }
+}
