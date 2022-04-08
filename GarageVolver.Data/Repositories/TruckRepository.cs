11f296e185b604835f16d1ba0953a4903e5b0e62
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-07 22:43:56 -0300

    created truck repository

---
+++ GarageVolver.Data/Repositories/TruckRepository.cs
@@ -0,0 +1,13 @@
+﻿using GarageVolver.Data.Context;
+using GarageVolver.Domain.Entities;
+using GarageVolver.Domain.Interfaces;
+
+namespace GarageVolver.Data.Repositories
+{
+    public class TruckRepository : BaseRepository<Truck>, ITruckRepository
+    {
+        public TruckRepository(SQLiteContext sQLiteContext) : base(sQLiteContext)
+        {
+        }
+    }
+}
