7325b95f43203091247de59eb1c1bcb20876674c
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-07 22:36:37 -0300

    added truck model helper

---
+++ GarageVolver.UnitTest/Helpers/TruckModelHelper.cs
@@ -0,0 +1,31 @@
+﻿using GarageVolver.Domain.Entities;
+using GarageVolver.Domain.Helpers;
+using System.Linq;
+using System;
+
+namespace GarageVolver.UnitTest.Helpers
+{
+    public static class TruckModelHelper
+    {
+        public static readonly Random _random = new();
+
+        public static TruckModel RandomTruckModel()
+        {
+            var listOfTruckModels = Enumeration.GetAll<TruckModel>().ToList();
+
+            return listOfTruckModels[_random.Next(listOfTruckModels.Count)];
+        }
+
+        public static int RandomTruckModelId()
+        {
+            var listOfTruckModels = Enumeration.GetAll<TruckModel>().ToList();
+
+            return _random.Next(listOfTruckModels.Count);
+        }
+
+        public static string RandomTruckModelName()
+        {
+            return RandomTruckModel().Name;
+        }
+    }
+}
