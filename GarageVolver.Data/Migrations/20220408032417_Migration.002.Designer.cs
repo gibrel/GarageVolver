79b7d7b935bd1983a3f84bc7d3dec9ed4d130f77
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-04-08 17:47:09 -0300

    added migration to include license plate on schema

--- GarageVolver.Data/Migrations/20220408032417_Migration.002.Designer.cs
+++ GarageVolver.Data/Migrations/20220408032417_Migration.002.Designer.cs
@@ -2,6 +2,7 @@
 using GarageVolver.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GarageVolver.Data.Migrations
 {
     [DbContext(typeof(SQLiteContext))]
-    partial class SQLiteContextModelSnapshot : ModelSnapshot
+    [Migration("20220408032417_Migration.002")]
+    partial class Migration002
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.3");
---
+++ GarageVolver.Data/Migrations/20220408032417_Migration.002.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GarageVolver.Data.Migrations
+{
+    public partial class Migration002 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Truck_LicencePlate",
+                table: "Truck");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Truck_LicencePlate",
+                table: "Truck",
+                column: "LicencePlate");
+        }
+    }
+}
