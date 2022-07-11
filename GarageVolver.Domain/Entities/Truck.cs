6a68d727bc5320c386837c9c7cf9bc80676ce0a2
Gabriel Marques Dalla Costa <gibrel@gmail.com>
2022-07-11 09:17:30 -0300

    added constructor to entity and removed commented code

--- GarageVolver.Domain/Entities/Truck.cs
+++ GarageVolver.Domain/Entities/Truck.cs
@@ -5,15 +5,21 @@ namespace GarageVolver.Domain.Entities
     public class Truck : BaseEntity
     {
         public TruckModel Model { get; set; }
-        public int ManufacturingYear { get; set; } = DateTime.Now.Year;
+        public int ManufacturingYear { get; set; }
         public int ModelYear { get; set; }
+
+        public Truck(TruckModel model, int manufacturingYear, int modelYear = 0)
+        {
+            Model = model;
+            ManufacturingYear = manufacturingYear;
+            ModelYear = modelYear == 0 ? DateTime.Now.Year : modelYear;                
+        }
     }
 
     public class TruckModel : Enumeration
     {
         public static readonly TruckModel FH = new(0, nameof(FH));
         public static readonly TruckModel FM = new(1, nameof(FM));
-        //public static readonly TruckModel FMX = new(2, nameof(FMX));
 
         public TruckModel(int id, string name) : base(id, name)
         {
